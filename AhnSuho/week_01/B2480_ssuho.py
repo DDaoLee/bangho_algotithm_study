31c663720cb35b16ec5b7ff55f1ae95888f39440
수호 <suho@suhoui-MacBookPro.local>
2025-08-03 14:56:51 +0900

    feat: B2480_ssuho

---
+++ AhnSuho/week_01/B2480_ssuho.py
@@ -0,0 +1,12 @@
+def Dicegame(a, b, c):
+    if a==b and b==c:
+        return 10000+a*1000
+    elif a==b or a==c:
+        return 1000+a*100
+    elif b==c: 
+        return 1000+b*100
+    else:
+        return max(a, b, c)*100
+
+a, b, c = map(int, input().split())
+print (Dicegame(a, b, c))
