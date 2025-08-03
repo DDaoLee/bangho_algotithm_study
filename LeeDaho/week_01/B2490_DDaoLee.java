b1450cbf3d356e2cb3bf74eaf7aa98b31d589c83
DDaoLee <dlekgh3576@naver.com>
2025-08-03 19:38:51 +0900

    feat: B2490_LeeDaho

---
+++ LeeDaho/week_01/B2490_DDaoLee.java
@@ -0,0 +1,25 @@
+package LeeDaho.week_01;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B2490_DDaoLee {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = {"D", "C", "B", "A", "E"};
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < 3; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            while (st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+            sb.append(arr[sum] + "\n");
+        }
+        System.out.print(sb);
+    }
+}
